1dc0bbf9cc2717b1a32124d8182450c3c9031b02
allan chiu <56469564+allanchiu@users.noreply.github.com>
2025-05-14 22:44:26 +0800

    new folder

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+from pprint import pprint
+
+app = Flask(__name__)
+
+import PureCloudPlatformClientV2
+from PureCloudPlatformClientV2.rest import ApiException
+
+CLIENT_ID = "70b17ec8-5475-47fb-b8fe-f8572f4d2b63"
+CLIENT_SECRET = "r9eNTdKJcHCURIMvDTuQ7b1AyIP8vgWTnwl6KqdjrJU"
+ENVIRONMENT = "https://api.euw2.pure.cloud"  
+
+
+apiclient = PureCloudPlatformClientV2.api_client.ApiClient()
+apiclient.host = ENVIRONMENT
+apiclient.get_client_credentials_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+
+
+# Create Conversation API Instance
+conversationApi = PureCloudPlatformClientV2.ConversationsApi(apiclient)
+
+
+@app.route("/get_media_url", methods=["GET"])
+def get_media_url():
+    # Create Rec API Instance
+    recordApi = PureCloudPlatformClientV2.RecordingApi(apiclient)
+    conversation_id = request.args.get("con_id")  # str | Conversation ID
+    max_wait_ms = 10000  # int | The maximum number of milliseconds to wait for the recording to be ready. Must be a positive value. (optional) (default to 5000)
+    format_id = "WAV"  # str | The desired media format. Valid values:WAV,WEBM,WAV_ULAW,OGG_VORBIS,OGG_OPUS,MP3,NONE. (optional) (default to 'WEBM')
+    media_formats = [
+        "WAV"
+    ]  # list[str] | All acceptable media formats. Overrides formatId. Valid values:WAV,WEBM,WAV_ULAW,OGG_VORBIS,OGG_OPUS,MP3. (optional)
+
+    try:
+        api_response = recordApi.get_conversation_recordings(
+            conversation_id=conversation_id,
+            max_wait_ms=max_wait_ms,
+            format_id=format_id,
+            media_formats=media_formats,
+        )
+        message = api_response[0].media_uris["0"].media_uri
+        return render_template("index.html", message=message)
+    except ApiException as e:
+        print(
+            "Exception when calling RecordingApi->get_conversation_recordings: %s\n" % e.status
+        )
+        return ('no recording')
+
+
+@app.route("/get_conversation", methods=["GET"])
+def get_conversation():
+    body = PureCloudPlatformClientV2.ConversationQuery()
+    segmentFilter = PureCloudPlatformClientV2.SegmentDetailQueryFilter()
+
+    predicte01 = PureCloudPlatformClientV2.SegmentDetailQueryPredicate()
+    predicte01.type = "dimension"
+    predicte01.dimension = "direction"
+    predicte01.operator = "matches"
+    predicte01.value = "inbound"
+    segmentFilter.predicates = [predicte01]
+    segmentFilter.type = "or"
+    body.segment_filters = [segmentFilter]
+    body.interval = "2025-03-25T00:00:00.000Z/2025-03-25T23:59:59.999Z"
+
+    try:
+        api_response = conversationApi.post_analytics_conversations_details_query(body)
+        response = api_response.to_dict()
+        conversations = []
+        total_hits = response["total_hits"]
+        for i in range(0, total_hits):
+            conversation = {
+                "conversation_id": response["conversations"][i]["conversation_id"]
+            }
+            conversations.append(conversation)
+        return render_template("conversation.html", conversations=conversations)
+    except ApiException as e:
+        print("Exception when calling ConversationApi %s\n" % e)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
